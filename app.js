c6c0a33b405a4fb27ecaef547248445a016f821d
ESTELA ALVES DE MORAES <23111781@CFP127ALUNO.LOCAL>
2023-08-16 10:03:37 -0300

    Tratamento de dados: letras, vírgula. Limitar casas decimais

--- app.js
+++ app.js
@@ -32,30 +32,34 @@ entradaDados.question('Digite o nome do aluno: ', function(nomeAluno){
 
             entradaDados.question('Nota 1: ', function(notaUm){
 
-                let nota1 = notaUm
+                let nota1 = notaUm.replace(',', '.')
 
                 entradaDados.question('Nota 2: ', function(notaDois){
 
-                    let nota2 = notaDois
+                    let nota2 = notaDois.replace(',', '.')
 
                     entradaDados.question('Nota 3: ', function(notaTres){
 
-                        let nota3 = notaTres
+                        let nota3 = notaTres.replace(',', '.')
 
                         entradaDados.question('Nota 4: ', function(notaQuatro){
 
-                            let nota4 = notaQuatro
+                            let nota4 = notaQuatro.replace(',', '.')
 
                             //validação de entrada das notas em branco
                             if(nota1 == '' || nota2 == '' || nota3 == '' || nota4 == ''){
                                 console.log('')
                                 console.log('>> ERRO: Todas as notas devem ser preenchidas.')
-                            }else{
-
-
-
-
-                            var media = (Number(nota1)+Number(nota2)+Number(nota3)+Number(nota4))/4
+                                entradaDados.close()
+                            }else if(isNaN(nota1) || isNaN(nota2) || isNaN(nota3) || isNaN(nota4)){
+                                console.log('>> ERRO: É obrigatório a entrada de dados somente com números.')
+                                entradaDados.close()
+                            } else if(nota1 > 10 || nota1 < 0 || nota2 > 10 || nota2 < 0 || nota3 > 10 || nota3 < 0|| nota4 > 10 || nota4 < 0){
+                                console.log('>> ERRO: É obrigatório inserir apenas notas entre 0 e 10.')
+                                entradaDados.close()
+                            } else {
+                    
+                            let media = (Number(nota1)+Number(nota2)+Number(nota3)+Number(nota4))/4
 
                             console.log('')
                             console.log('+-----------------')
@@ -66,19 +70,19 @@ entradaDados.question('Digite o nome do aluno: ', function(nomeAluno){
                             console.log('+-----------------')
                             console.log(`| Disciplina: ${disciplina}`)
                             console.log('+-----------------')
-                            console.log(`| Nota 1: ${nota1} `)
-                            console.log(`| Nota 2: ${nota2} `)
-                            console.log(`| Nota 3: ${nota3} `)
-                            console.log(`| Nota 4: ${nota4} `)
-                            console.log(`| Media: ${media} `)
+                            console.log(`| Nota 1: ${String(nota1).replace('.',',')} `)
+                            console.log(`| Nota 2: ${String(nota2).replace('.',',')} `)
+                            console.log(`| Nota 3: ${String(nota3).replace('.',',')} `)
+                            console.log(`| Nota 4: ${String(nota4).replace('.',',')} `)
+                            console.log(`| Media: ${String(media.toFixed(1)).replace('.',',')} `)
                             console.log('+-----------------')
                             entradaDados.close()
-
                             }
+
+                        })
                         })
                     })
                 })
             })
         })
     })
-})
